7a97b869789839facf104aae24b9bde3f071373c
AkashMAbrrar <madborakash48@gmail.com>
2024-11-26 18:50:53 +0600

    this commit for the update user

--- src/components/Users.jsx
+++ src/components/Users.jsx
@@ -1,7 +1,26 @@
-import { useLoaderData } from "react-router-dom";
+import { useState } from "react";
+import { Link, useLoaderData } from "react-router-dom";
 
 const Users = () => {
-  const users = useLoaderData();
+  const loadedUsers = useLoaderData();
+  const [users, setUsers] = useState(loadedUsers);
+
+  const handleDelete = (_id) => {
+    console.log("delete by id", _id);
+    fetch(`http://localhost:5000/users/${_id}`, {
+      method: "DELETE",
+    })
+      .then((res) => res.json())
+      .then((data) => {
+        console.log(data);
+        if (data.deletedCount > 0) {
+          alert("deleted successfully");
+          const remaining = users.filter((user) => user._id !== _id);
+          setUsers(remaining);
+        }
+      });
+  };
+
   console.log(users);
   return (
     <div>
@@ -9,7 +28,11 @@ const Users = () => {
       <div>
         {users.map((user) => (
           <p key={user._id}>
-            {user.name}: {user.email}
+            {user.name}:{user.email}
+            <Link to={`/update/${user._id}`}>
+              <button>Update</button>
+            </Link>
+            <button onClick={() => handleDelete(user._id)}>X</button>
           </p>
         ))}
       </div>
